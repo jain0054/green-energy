095a38a750f225960978ff04ffff12c595c46677
jain0054 <122372524+jain0054@users.noreply.github.com>
2023-04-05 12:50:43 -0400

    js created

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// Define the services as an object
+const services = {
+    1: {
+        title: "Solar Energy",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        image: "./img/solar.jpg" 
+    },
+    2: {
+        title: "Hydro Energy",
+        content: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.",
+        image: "./img/hydro.jpg" 
+    },
+    3: {
+        title: "Geothermal Energy",
+        content: "Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu.",
+        image: "./img/geo.jpg" 
+    }
+  };
+    // Get the buttons and content
+    const btn = document.querySelectorAll(".manipulation button");
+    const content = document.querySelector(".post");
+    
+    // Loop through the buttons and add a click event listener
+    btn.forEach(function(button) {
+      button.addEventListener("click", function() {
+          // Remove the active class from all buttons
+          btn.forEach(function(btn) {
+              btn.classList.remove("active-button");
+          });
+          // Add the active class to the clicked button
+          button.classList.add("active-button");
+          // Get the ID of the clicked button
+  const id = button.getAttribute("id").slice(-1);
+  // Get the service from the services object
+  const service = services[id];
+  // Update the content
+  // content.innerHTML = `<h2>${service.title}</h2><p>${service.content}</p><img src="${service.image}" alt="${service.title} Image">`;
+  content.innerHTML = `
+  <h1>${service.title}</h1>
+  <img src="${service.image}" alt="${service.title} Image">
+  <p>${service.content}</p>`;
+  
+      });
+    });
+  
+  // Set the first button to active by default
+  btn[0].classList.add("active-button");
+  
+  // Responsive design
+  const mediaQuery = window.matchMedia('(min-width: 30rem)');
+  const container = document.querySelector('.container');
+  const masthead = document.querySelector('.masthead');
+  
+  function handleMediaQuery(mediaQuery) {
+  if (mediaQuery.matches) {
+    container.style.fontSize = '1.2rem';
+    masthead.style.height = '50vh';
+  } else {
+    container.style.fontSize = '1rem';
+    masthead.style.height = '30rem';
+  }
+  }
+  
+  handleMediaQuery(mediaQuery);
+  mediaQuery.addListener(handleMediaQuery);
+  
+  
